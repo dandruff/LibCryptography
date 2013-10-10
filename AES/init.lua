68df8dd73f188a90532d70bc23d37d98bc65b435
Cimplex <reubend@formetco.com>
2013-10-10 16:32:34 -0500

    Initial commit

---
+++ AES/init.lua
@@ -0,0 +1,7 @@
+-- Added by Dandruff
+local Name, Public = ...
+
+-- Create a temporary handle for our functions
+-- You should clean this up when your addon finishes initiallizing
+Public.AES = { }
+
